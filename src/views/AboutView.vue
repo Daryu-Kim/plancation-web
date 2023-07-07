e0adbdc17f17137014d3eef34fd01c74bb9fa150
guruma99 <bowbow_ovo@naver.com>
2023-07-08 05:06:21 +0900

    🚧 스크롤이벤트 진행중

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -8,19 +8,12 @@
       </h1>
 
       <nav>
+        <!-- 👇 nav바 리스트 / 해당영역으로 스크롤 부드럽게 이동-->
         <ul class="gnbLog fs_9">
-          <li>
-            <a href="#section2">캘린더/To-do</a>
-          </li>
-          <li>
-            <a href="#section3">AutoSchedule</a>
-          </li>
-          <li>
-            <a href="#section4">기록/다이어리</a>
-          </li>
-          <li>
-            <a href="#section1">설치</a>
-          </li>
+          <li @click="scrollToSection('section2')">캘린더/To-do</li>
+          <li @click="scrollToSection('section3')">AutoSchedule</li>
+          <li @click="scrollToSection('section4')">기록/다이어리</li>
+          <li @click="scrollToSection('section1')">설치</li>
         </ul>
       </nav>
     </header>
@@ -37,8 +30,8 @@
         </div>
       </section>
 
-      <section id="section1">
-        <div class="TextWrap">
+      <section ref="section1" id="section1">
+        <div class="TextWrap reveal" ref="reveal">
           <p class="fs_21">설치</p>
           <p class="fs_12">모바일에서도 플랜케이션을 사용할 수 있어요.</p>
           <p class="fs_12">
@@ -55,8 +48,8 @@
         </div>
       </section>
 
-      <section id="section2">
-        <div class="sectionWrap">
+      <section ref="section2" id="section2">
+        <div class="sectionWrap reveal" ref="reveal">
           <div class="aboutTextWrap">
             <h2 class="fs_19">캘린더/ to-do list</h2>
             <p class="fs_13">일정관리의 기본이 되는</p>
@@ -64,50 +57,24 @@
           </div>
 
           <div class="aboutImgwrap">
-            <img src="@/assets/images/aboutCalenderImg1.png" alt="캘린더 이미지1" />
-            <img src="@/assets/images/aboutCalenderImg2.png" alt="캘린더 이미지2" />
+            <img :src="images1[imageIndex]" alt="캘린더 이미지1" />
+            <img :src="images2[imageIndex]" alt="캘린더 이미지2" />
           </div>
 
           <div class="slideDataWrap">
-            <p class="fs_11">중요한 일정을 강조하세요.</p>
-            <p class="fs_11">원하는 색의 마크업, 이모지로 한눈에 확인 할 수 있어요.</p>
+            <p class="fs_11">{{ text1[textIndex] }}</p>
+            <p class="fs_11">{{ text2[textIndex] }}</p>
 
             <div class="slideBtn">
-              <button class="fs_8">1</button>
-              <button class="fs_8">2</button>
+              <button @click="changeItem1" class="fs_8">1</button>
+              <button @click="changeItem2" class="fs_8">2</button>
             </div>
           </div>
         </div>
       </section>
 
-      <!-- <section>
-        <div class="sectionWrap">
-          <div class="aboutTextWrap">
-            <h2 class="fs_19">캘린더/ to-do list</h2>
-            <p class="fs_13">일정관리의 기본이 되는</p>
-            <p class="fs_13">캘린더와 해야 할 일 관리</p>
-          </div>
-
-          <div class="aboutImgwrap">
-            <img src="@/assets/images/aboutTodoImg1.png" alt="todo 이미지1" />
-            <img src="@/assets/images/aboutTodoImg2.png" alt="todo 이미지2" />
-          </div>
-
-          <div class="slideDataWrap">
-            <div>
-              <p class="fs_11">해야 할 일들에 대해 간결하게 관리하세요.</p>
-              <p class="fs_11">시간과 알림, 장소, 참여자에 대한 관리도 가능합니다.</p>
-            </div>
-            <div>
-              <button>1</button>
-              <button>2</button>
-            </div>
-          </div>
-        </div>
-      </section> -->
-
-      <section id="section3">
-        <div class="sectionWrap">
+      <section ref="section3" id="section3">
+        <div class="sectionWrap reveal" ref="reveal">
           <div class="aboutTextWrap">
             <h2 class="fs_19">Auto schedule</h2>
             <p class="fs_13">짜기 어려운 계획을 대신 해결해주는</p>
@@ -126,8 +93,8 @@
         </div>
       </section>
 
-      <section id="section4">
-        <div class="sectionWrap">
+      <section ref="section4" id="section4">
+        <div class="sectionWrap reveal" ref="reveal">
           <div class="aboutTextWrap">
             <h2 class="fs_19">다이어리/기록</h2>
             <p class="fs_13">오늘 하루 무슨 일이 있었나요?</p>
@@ -153,8 +120,135 @@
 @import '../assets/scss/pages/about.scss';
 @import '../assets/scss/abstracts/Fontmodule.css';
 </style>
+
 <script lang="ts">
-// const $style = useCssModule()
-// import css from '../assets/scss/pages/about.css'
-// import { useCssModule } from 'vue'
+import { defineComponent } from 'vue'
+
+window.addEventListener('scroll', reveal)
+
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal')
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowheight = window.innerHeight
+    var revealtop = reveals[i].getBoundingClientRect().top
+    var revealpoint = 150
+
+    console.log(revealtop)
+
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add('active')
+    } else {
+      reveals[i].classList.remove('active')
+    }
+  }
+}
+
+export default defineComponent({
+  components: {},
+  data() {
+    return {
+      // 이미지 변경
+      images1: ['src/assets/images/aboutCalenderImg1.png', 'src/assets/images/aboutTodoImg1.png'],
+      images2: ['src/assets/images/aboutCalenderImg2.png', 'src/assets/images/aboutTodoImg2.png'],
+
+      imageIndex: 0,
+
+      // 텍스트 변경
+      text1: [
+        '원하는 색의 마크업, 이모지로 한눈에 확인 할 수 있어요.',
+        '시간과 알림, 장소, 참여자에 대한 관리도 가능합니다.'
+      ],
+      text2: ['중요한 일정을 강조하세요.', '해야 할 일들에 대해 간결하게 관리하세요.'],
+
+      textIndex: 0,
+
+      // 스크롤 이벤트 페이드인 & 페이드아웃
+      showNavbar: true,
+      lastScrollPosition: 0
+    }
+  },
+
+  methods: {
+    // 1번 버튼: Calendar이미지와 텍스트
+    changeItem1() {
+      this.imageIndex = 0
+      this.textIndex = 0
+    },
+
+    // 2번 버튼: Todo이미지와 텍스트
+    changeItem2() {
+      this.imageIndex = 1
+      this.textIndex = 1
+    }
+
+    // 👇스크롤 이벤트 페이드인 & 페이드아웃
+    // setReveal() {
+    //   var reveals = document.querySelectorAll('.reveal')
+
+    //   for (var i = 0; i < reveals.length; i++) {
+    //     var windowheight = window.innerHeight
+    //     var revealtop = reveals[i].getBoundingClientRect().top
+    //     var revealpoint = 150
+
+    //     console.log(revealtop)
+
+    //     if (revealtop < windowheight - revealpoint) {
+    //       reveals[i].classList.add('active')
+    //     } else {
+    //       reveals[i].classList.remove('active')
+    //     }
+    //   }
+    // }
+  },
+
+  setup() {
+    // const revealRefs = ref<(HTMLElement | null)[]>([])
+
+    // const addRevealRef = (el: HTMLElement | null) => {
+    //   if (el && !revealRefs.value.includes(el)) {
+    //     revealRefs.value.push(el)
+    //   }
+    // }
+
+    // const setReveal = () => {
+    //   const windowHeight = window.innerHeight
+    //   const revealPoint = 150
+
+    //   if (reveal) {
+    //     const revealTop = revealEl.getBoundingClientRect().top
+
+    //     if (revealTop < windowHeight - revealPoint) {
+    //       revealEl.classList.add('active')
+    //     } else {
+    //       revealEl.classList.remove('active')
+    //     }
+    //   }
+    // }
+
+    // const onScroll = () => {
+    //   setReveal()
+    // }
+
+    // onMounted(() => {
+    //   window.addEventListener('scroll', onScroll)
+    // })
+
+    // onUnmounted(() => {
+    //   window.removeEventListener('scroll', onScroll)
+    // })
+
+    const scrollToSection = (refName: string) => {
+      const sectionElement = document.getElementById(refName)
+
+      if (sectionElement) {
+        sectionElement.scrollIntoView({
+          behavior: 'smooth'
+        })
+      }
+    }
+
+    return { scrollToSection }
+  }
+})
 </script>
