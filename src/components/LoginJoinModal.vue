c82a87d8bb918036e46a562a27ce9670c80a6f61
guruma99 <bowbow_ovo@naver.com>
2023-07-10 23:50:18 +0900

    🚧 로그인모달 스타일링중

--- src/components/LoginJoinModal.vue
+++ src/components/LoginJoinModal.vue
@@ -1,4 +1,43 @@
-<template lang="">
-  <div></div>
+<template>
+  <div class="LoginJoinModal">
+    <div class="form">
+      <div class="header">
+        <h1>Log into your</h1>
+        <h1>Account</h1>
+      </div>
+
+      <div class="emailInput">
+        <input type="text" placeholder="">
+        <span class="loginText emailTextId">사용자이메일</span>
+        <span class="loginText emailWran">이메일형식 오류</span>
+      </div>
+
+      <div class="pwInput">
+        <input type="password" placeholder="">
+        <span class="loginText loginTextPw">비밀번호</span>
+        <span class="passwordWranF">비밀번호 표시</span>
+        <span class="passwordWranL">8-12자내로 입력해주세요.</span>
+        <!-- <span class="password-wran off">숨기기</span> -->
+      </div>
+
+      <a class="getPassword" href="">비밀번호를 잊으셨나요?</a>
+
+      <button type="button" class="loginBtn">로그인</button>
+
+      <div class="line-box">
+        <div class="line"></div>
+        <p>다른방법으로 로그인</p>
+        <div class="line"></div>
+      </div>
+
+
+      <button type="button">
+        <img src="@/assets/images/LoginJoin/GoogleLogo.png" alt="구글로고">
+        <p>Google로 로그인</p>
+      </button>
+    </div>
+  </div>
 </template>
-<style lang="ts"></style>
+<style lang="scss">
+@import '../assets/scss/components/LoginJoinModal.css';
+</style>
--- src/views/LoginJoinView.vue
+++ src/views/LoginJoinView.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="loginJoin">
-    <h1>loginJoin</h1>
+    <!-- <login-join-modal></login-join-modal> -->
+    <LoginJoinModal></LoginJoinModal>
   </div>
 </template>
+<script lang="ts">
+import LoginJoinModal from "../components/LoginJoinModal.vue";
+
+export default {
+  components: {
+    LoginJoinModal,
+  }
+}
+
+</script>
+<style lang="scss">
+@import '../assets/scss/pages/loginJoin.css';
+@import '../assets/scss/abstracts/Fontmodule.css';
+</style>
