5d34892c4d0502640d2b0fe71d033e0dd47296f8
guruma99 <bowbow_ovo@naver.com>
2023-07-13 01:00:42 +0900

    💄 사이드바&헤더 스타일링

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,9 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <style lang="scss">
 * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
   font-family: 'Pretendard-Medium';
 }
 </style>
