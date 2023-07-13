0ae69e4e8639459b17b7af1b78f4afa0bf4acd4e
guruma99 <bowbow_ovo@naver.com>
2023-07-14 02:18:16 +0900

    ♻️ 알림 컴포넌트

---
+++ src/components/NotifyItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="notifyWrap">
+      <div class="notifyBox">
+        <img src="../assets/images/main/icon_todo.svg" alt="알림의 아이콘">
+        <p>과제 제출 <span class="fs_9">5월 12일</span></p>
+        <p>'과제 제출' 까지 7일 남았습니다.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '../assets/scss/abstracts/Fontmodule.css';
+</style>
+
+
+
