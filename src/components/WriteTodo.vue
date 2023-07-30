c39089e94eaa6dfcae04789c10058372df8d1614
guruma99 <bowbow_ovo@naver.com>
2023-07-30 20:35:29 +0900

    todo참여자 유저 배열넣고빼기

--- src/components/WriteTodo.vue
+++ src/components/WriteTodo.vue
@@ -3,7 +3,7 @@
     <div class="todoHeader">
       <p>할 일 추가</p>
       <!-- <img src="../assets/images/main/icon_close.svg" alt="취소 아이콘"> -->
-      <button @click="saveToFirestore">등록</button>
+      <button @click="__addTodo">등록</button>
     </div>
 
     <div class="writeBox">
@@ -47,9 +47,6 @@
           </div>
         </li>
 
-        <!-- <li>
-
-        </li> -->
         <li class="alert">
           <p>알림</p>
           <!-- <알림의 모달띄울곳> -->
@@ -60,17 +57,17 @@
         <li class="addUser">
           <p class="addUserTitle">참여자 추가</p>
 
-          <label v-for="(user, index) in state.calendarUser" :key="index" :for="'userCheck' + index">
+          <label v-for="(user, index) in state.userProfile" :key="index" :for="'userCheck' + index">
             <div class="userInfo">
-              <div class="userImage"
-                :style="{ 'background-image': 'url(https://firebasestorage.googleapis.com/v0/b/plancation-74a7a.appspot.com/o/Apps%2Fdefault_user_image.png?alt=media&token=24c09b27-9fd8-4604-8900-3f9c16c14452)' }">
+              <div class="userImage" :style="{ 'background-image': `url(${user.user.userImagePath})` }">
               </div>
-              <p>{{ user.nickname }}</p>
+              <p>{{ user.user.userName }}</p>
             </div>
 
             <img v-show="!user.checked" src="../assets/images/main/icon_userEmptyBox.svg" alt="체크없는박스">
             <img v-show="user.checked" src="../assets/images/main/icon_userCheckBox.svg" alt="체크박스">
-            <input :id="'userCheck' + index" type="checkbox" v-model="user.checked" class="userCheck">
+            <input :id="'userCheck' + index" type="checkbox" v-model="user.checked" v-on:change="checkedUser(user)"
+              class="userCheck">
           </label>
         </li>
       </ul>
@@ -82,9 +79,9 @@
 <script setup lang="ts">
 import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css';
-import { ref, reactive, onMounted } from 'vue';
+import { ref, reactive, onMounted, useTransitionState } from 'vue';
 import { getAuth } from "firebase/auth";
-import { doc, addDoc, getDoc, getFirestore, collection, Timestamp } from 'firebase/firestore';
+import { doc, getDoc, getFirestore, Timestamp, addDoc, collection } from 'firebase/firestore';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
@@ -99,24 +96,42 @@ const state = reactive({
   calendarUser: [] as any[],
   postTitle: '',
   calendarID: route.params.id, // 현재 캘린더 id
+  userProfile: [] as any[], //유저의 닉네임,사진,체크유무 파악
+  toDoUsers: [] as any[],
 });
 
-// const users = ref([
-//   { nickname: '강해민', checked: false },
-//   { nickname: '여상현', checked: false },
-//   { nickname: '김원재', checked: false },
-//   { nickname: '백승호', checked: false },
-// ]);
-
 // 메서드 정의
-
-//할일 제목받아오기
+// 할일 제목받아오기
 const setPostTitle = (e: any) => {
   var writeTitle = e.target.value;
   state.postTitle = writeTitle;
-  console.log(state.postTitle);
-  console.log(state.calendarUser)
+  // console.log(state.postTitle);
 };
+
+// const checkedUser = (user: any) => {
+//   if (user.checked) {
+//     state.toDoUsers = user.user.userID
+//     // console.log(user.user.userID);
+//   } else {
+//     console.log(user.user.userName + "이 체크 해제되었음")
+//   }
+//   console.log(state.toDoUsers)
+// };
+
+const checkedUser = (user: any) => {
+  if (user.checked) {
+    state.toDoUsers.push(user.user.userID);
+  } else {
+    const index = state.toDoUsers.indexOf(user.user.userID);
+    if (index > -1) {
+      state.toDoUsers.splice(index, 1);
+    }
+    console.log(user.user.userName + "이 체크 해제되었음")
+  }
+  console.log(state.toDoUsers)
+};
+
+
 
 //현재 캘린더의 참여유저들 알아오기
 const getCalendars = async () => {
@@ -126,12 +141,13 @@ const getCalendars = async () => {
 
   if (docSnap.exists()) {
     state.calendarUser = docSnap.data().calendarUsers.map((user) => ({
-      nickname: user,
-      checked: false,
+      userUID: user,
     }));
+    // state.calendarUser.push(docSnap.data());
   } else {
     console.log('No such document!');
   }
+  return console.log(state.calendarUser)
 };
 
 //date타입으로 변환
@@ -144,23 +160,66 @@ const combineDateTime = (date, time) => {
   }
 };
 
+
+// 새로 추가한 함수: state.calendarUser의 배열에 담긴 유저uid로 파이어베이스에서 유저 uid가 포함된 컬렉션을 찾는 함수
+const findUserCollections = async () => {
+  const db = getFirestore();
+  state.calendarUser.forEach(async (userObj: any) => {
+    const userUid = userObj.userUID;
+
+    const userDoc = doc(db, "Users", userUid);
+    try {
+      const docSnap = await getDoc(userDoc)
+        .then((docSnap) => {
+          // state.userProfile.user.push(docSnap.data())
+          // state.userProfile = docSnap.data().map((user) => ({
+          //   user: user,
+          //   checked: false,
+          // }));
+          state.userProfile.push({
+            user: docSnap.data(),
+            checked: false
+          });
+
+        })
+        .catch((e) => { console.log(e) })
+    }
+    catch (e) {
+      console.log(e)
+    }
+    return console.log(state.userProfile)
+  });
+};
+
+
 //파이어스토어에 todo등록
-const saveToFirestore = async () => {
-  // console.log(sDate.value, sTime.value);
+const __addTodo = async () => {
+  console.log(sDate.value, sTime.value);
   const combinedStartDate = combineDateTime(sDate.value, sTime.value);
   const combinedEndDate = combineDateTime(eDate.value, eTime.value);
-  // try {
-  //   const auth = getAuth()
-  //   const user: any = auth.currentUser
-  //   const db = getFirestore();
 
-  //   //랜덤 ID로 문서생성
-  //   const docRef: any = await addDoc(collection(db, `Calendars/${state.calendarID}/Todos`,), {
+  try {
+    const auth = getAuth()
+    const user: any = auth.currentUser
+    const db = getFirestore();
 
-  //   })
-
-  //   //     Todos
-
+    //랜덤 ID로 문서생성
+    const docRef: any = await addDoc(collection(db, `Calendars/${state.calendarID}/Todos`), {
+      todoAlerts: "없음",
+      todoAuthorID: user.uid,
+      todoCheckUsers: [],
+      todoEndTime: combinedEndDate,
+      todoStartTime: combinedStartDate,
+      todoTitle: state.postTitle,
+      todoUsers: [],
+      todoID: "test"
+    }).then(() => {
+      alert("할 일이 추가되었습니다!")
+    })
+      .catch((e) => { alert(e.message) })
+  } catch (err) {
+    console.log(err.message)
+  }
   //   // - todoAlerts: number = 할 일 알림 (0.없음 1.당일 2.하루전 3.일주일전)
   //   // - todoAuthorID: string = 할 일 게시자 UID
   //   // - todoCheckUsers: string[] = 할 일 체크한 유저들의 UID
@@ -170,116 +229,18 @@ const saveToFirestore = async () => {
   //   // - todoUsers: string[] = 할 일 대상 유저들의 UID
   //   // - todoID: string = 할 일 ID
 
-  // } catch (err) {
-  //   console.log(err)
-  // }
-  // const scheduleData = {
-  //   startDate: combinedStartDate,
-  //   endDate: combinedEndDate,
-  // };
-
-  console.log(combinedStartDate, combinedEndDate);
+  console.log(combinedStartDate, combinedEndDate, state.userProfile);
 };
 
+
+
 // 라이프사이클훅을 핸들링할 때 Composition API를 사용합니다.
-onMounted(() => {
-  getCalendars();
+onMounted(async () => {
+  await getCalendars();
+  await findUserCollections();
 });
 
 </script>
-
-
-<!-- <script lang="ts">
-// import { getAuth } from "firebase/auth";
-import { doc, getDoc, getFirestore } from "firebase/firestore";
-export default {
-  data() {
-    return {
-      calendarUser: [] as any[],
-      postTitle: "",
-      calendarID: this.$route.params.id, //현재 캘린더 id
-    }
-  },
-  created() {
-    //컴포넌트가 생성되자마자 현재 로그인한 사용자의 프로필 정보 가져와 표시
-    this.getCalendars();
-  },
-
-  methods: {
-    //타이틀 값 받아내기
-    setPostTitle(e: any) {
-      var writeTitle = e.target.value;
-      this.postTitle = writeTitle
-      console.log(this.postTitle)
-    },
-
-    //현재캘린더 참여자 목록 조회
-    async getCalendars() {
-      const db = getFirestore();
-      const yourCalendars = doc(db, "Calendars", this.calendarID);
-      const docSnap = await getDoc(yourCalendars);
-
-      if (docSnap.exists()) {
-        this.calendarUser = docSnap.data().calendarUsers
-        console.log(this.calendarUser)
-      } else {
-        console.log("No such document!");
-      }
-    }
-  }
-}
-</script>
-
-<script setup lang="ts">
-import VueDatePicker from '@vuepic/vue-datepicker';
-import '@vuepic/vue-datepicker/dist/main.css'
-import { ref } from 'vue';
-
-
-// const sDate = ref(new Date());
-const sDate = ref();
-const eDate = ref();
-const sTime = ref();
-const eTime = ref();
-
-const users = ref([
-  // { nickname: '강해민', checked: false },
-  // { nickname: '여상현', checked: false },
-  // { nickname: '김원재', checked: false },
-  // { nickname: '백승호', checked: false },
-]);
-
-// 추가된 함수: 선택한 날짜와 시간을 Date 형식으로 합치는 combineDateTime 함수
-const combineDateTime = (date, time) => {
-  if (date && time) {
-    const formattedDateTime = `${date}T${time}`;
-
-    return new Date(formattedDateTime);
-  } else {
-    return null;
-  }
-};
-
-// 추가된 함수: 선택한 날짜와 시간을 합치고, 필요한 정보와 함께 Firestore에 저장하는 saveToFirestore 함수
-const saveToFirestore = () => {
-  console.log(sDate.value, sTime.value)
-  const combinedStartDate = combineDateTime(sDate.value, sTime.value);
-  const combinedEndDate = combineDateTime(eDate.value, eTime.value);
-
-  // ... 여기에 Firestore에 저장할 추가 데이터를 작성합니다.
-
-  const scheduleData = {
-    startDate: combinedStartDate,
-    endDate: combinedEndDate,
-    // ... 추가 데이터
-  };
-
-  // ... 여기에 Firestore에 scheduleData를 저장하는 코드를 작성합니다.
-  console.log(combinedStartDate, combinedEndDate)
-};
-
-
-</script> -->
 
 <style lang="scss">
 @import '../assets/scss/components/WriteTodo.css';
